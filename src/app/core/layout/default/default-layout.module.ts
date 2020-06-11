840efc017ca0d68ef3a1b2b74505f285ef15f4cf
Pierre Demailly <pierredemailly.pro@gmail.com>
2020-06-11 13:22:24 +0200

    create user navigation components

--- src/app/core/layout/default/default-layout.module.ts
+++ src/app/core/layout/default/default-layout.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DefaultLayoutRoutingModule } from './default-layout-routing.module';
 import { DefaultLayoutComponent } from './default-layout.component';
 import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
+import { UserHeaderComponent } from '../../navigation/user-header/user-header.component';
+import { UserFooterComponent } from '../../navigation/user-footer/user-footer.component';
 
 @NgModule({
   declarations: [
     DefaultLayoutComponent,
+    UserHeaderComponent,
+    UserFooterComponent,
   ],
   imports: [
     CommonModule,
