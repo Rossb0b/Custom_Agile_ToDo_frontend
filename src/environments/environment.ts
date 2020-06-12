51606c0c11f8dea8ce423fe25b275a54fbe24fdf
Pierre Demailly <pierredemailly.pro@gmail.com>
2020-06-12 10:33:19 +0200

    add back url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://clubagilebackend-env.eba-xpqhfki4.us-east-2.elasticbeanstalk.com/api',
 };
 
 /*
